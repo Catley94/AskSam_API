e22ec94cf8f2e313ec55f0a6d4532813fb6365ef
positive-season <scp94373@gmail.com>
2024-04-04 00:32:51 +0100

    Testing updating an existing record

--- obj/Debug/net8.0/AskSam_API.AssemblyInfo.cs
+++ obj/Debug/net8.0/AskSam_API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AskSam_API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1da7054c617eebaaac8aff2a73d4488560142027")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dc1fee28482f680dbab17c4d3c43438b37e1d005")]
 [assembly: System.Reflection.AssemblyProductAttribute("AskSam_API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AskSam_API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
